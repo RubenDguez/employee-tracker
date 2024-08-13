668b4bcb05a7dcec2a357340213537671dae99fd
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-13 19:54:14 -0400

    update: sql files to achieve ACID

--- src/db/sql/seeds.sql
+++ src/db/sql/seeds.sql
@@ -1,25 +1,45 @@
-INSERT INTO department (name) 
-VALUES 
-    ('Store'),
-    ('Electric Guitars'),
-    ('Keyboards'),
-    ('Acoustic Guitars'),
-    ('Recording'),
-    ('Amps'),
-    ('DJ Gear'),
-    ('Effects'),
-    ('Drums'),
-    ('Basses'),
-    ('Accessories'),
-    ('Live Sound'),
-    ('Used');
+DO $$
+    BEGIN
+
+        INSERT INTO department (name) 
+        VALUES 
+            ('Store'),
+            ('Electric Guitars'),
+            ('Keyboards'),
+            ('Acoustic Guitars'),
+            ('Recording'),
+            ('Amps'),
+            ('DJ Gear'),
+            ('Effects'),
+            ('Drums'),
+            ('Basses'),
+            ('Accessories'),
+            ('Live Sound'),
+            ('Used');
+
+        INSERT INTO role (title, salary, department_id)
+        VALUES 
+            ('Cashier', 30000, 1),
+            ('Luthier', 65000, 2);
+
+        INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES
+            ('Argenis', 'Dominguez', 2, 1);
+
+    RAISE NOTICE 'TRANSACTION COMPLETED';
+
+    EXCEPTION WHEN OTHERS THEN
+        RAISE NOTICE 'AN ERROR HAS OCCURRED: %s', SQLERRM;
+        ROLLBACK;
+
+END $$;
 
 
-INSERT INTO role (title, salary, department_id)
-VALUES 
-    ('Cashier', 30000, 1),
-    ('Luthier', 65000, 2);
+SELECT role.id, role.title, role.salary, department.name AS department_name, role.created_at, role.updated_at
+FROM role
+JOIN department ON role.department_id = department.id;
 
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES
-    ('Argenis', 'Dominguez', 2, 1);
+SELECT role.id, role.title, role.salary, department.name AS department_name, role.created_at, role.updated_at
+FROM role
+JOIN department ON role.department_id = department.id
+WHERE role.id = 1;
