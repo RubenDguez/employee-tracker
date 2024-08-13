7a0f04747d85f5d4f416d668618d97714894709a
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-13 08:57:40 -0400

    implemented singleton pattern DB class
    Preventing unnecessary connections

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,11 +1,14 @@
-import { Pool, QueryResult } from 'pg';
+import { Pool, PoolClient } from 'pg';
 import * as dotenv from 'dotenv';
 import path from 'path';
 
 dotenv.config({ path: path.join(process.cwd(), 'db.env') });
 
 export default class DB {
-	private readonly pool = new Pool({
+	static #instance: DB | null = null;
+	#connection: PoolClient | null = null;
+
+	readonly pool = new Pool({
 		user: process.env.DB_USER,
 		password: process.env.DB_PASSWORD,
 		host: process.env.DB_HOST,
@@ -13,28 +16,27 @@ export default class DB {
 		port: parseInt(process.env.DB_PORT!),
 	});
 
-	/**
-	 * Connect
-	 * @return {Promise<void>}
-	 */
-	protected async connect(): Promise<void> {
-		try {
-			await this.pool.connect();
-		} catch (err) {
-			const ERROR = <Error>err;
-			throw new Error(ERROR.message);
-		}
+	// Private constructor for singleton pattern
+	// Preventing unnecessary connections
+	private constructor() {}
+
+	static getInstance() {
+		if (!this.#instance) this.#instance = new DB();
+		return this.#instance;
 	}
 
 	/**
-	 * Fetch
-	 * @param {string} query
-	 * @return {Promise<QueryResult<any>>}
+	 * Connect
+	 * @return {Promise<PoolClient>}
+	 * @description Connect to the database
 	 */
-	protected async fetch(query: string, values?: any): Promise<QueryResult<any>> {
+	async connection(): Promise<PoolClient> {
 		try {
-			const res = await this.pool.query(query, values);
-			return res;
+			if (this.#connection) return this.#connection;
+
+			this.#connection = await this.pool.connect();
+			console.log('DB is now connected...');
+			return this.#connection;
 		} catch (err) {
 			const ERROR = <Error>err;
 			throw new Error(ERROR.message);
