d078810f02da6be87d9ee747755c3534128e113e
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-13 20:38:06 -0400

    update: queries

--- src/db/sql/seeds.sql
+++ src/db/sql/seeds.sql
@@ -19,12 +19,13 @@ DO $$
 
         INSERT INTO role (title, salary, department_id)
         VALUES 
+            ('Manager', 65000, 1),
             ('Cashier', 30000, 1),
             ('Luthier', 65000, 2);
 
         INSERT INTO employee (first_name, last_name, role_id, manager_id)
         VALUES
-            ('Argenis', 'Dominguez', 2, 1);
+            ('Argenis', 'Dominguez', 1, null);
 
     RAISE NOTICE 'TRANSACTION COMPLETED';
 
@@ -43,3 +44,18 @@ SELECT role.id, role.title, role.salary, department.name AS department_name, rol
 FROM role
 JOIN department ON role.department_id = department.id
 WHERE role.id = 1;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_name, 
+    CASE WHEN manager.id IS NULL THEN 'NONE' ELSE CONCAT(manager.first_name, ' ', manager.last_name) END AS manager_full_name, 
+    employee.created_at, employee.updated_at
+FROM employee
+JOIN role ON employee.role_id = role.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
+
+SELECT employee.id, employee.first_name, employee.last_name, role.title AS role_name, 
+    CASE WHEN manager.id IS NULL THEN 'NONE' ELSE CONCAT(manager.first_name, ' ', manager.last_name) END AS manager_full_name, 
+    employee.created_at, employee.updated_at
+FROM employee
+JOIN role ON employee.role_id = role.id
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
+WHERE employee.id = 1;
