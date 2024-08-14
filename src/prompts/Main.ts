b823fef3d0a71b375676f61c9e2a2730d0483b04
Ruben Dominguez <71775213+RubenDguez@users.noreply.github.com>
2024-08-14 13:07:07 -0400

    update: default options based on role (#7)

--- src/prompts/Main.ts
+++ src/prompts/Main.ts
@@ -2,31 +2,46 @@ import inquirer from 'inquirer';
 import State, { EState } from '../store/state';
 
 const main = () => {
+	const ROLE = State.getInstance().get(EState.ROLE);
+
+	// Default options
 	let choices = [
 		'View all departments',
 		'View all roles',
 		'View all employees',
 		new inquirer.Separator(),
-		'Add a department',
-		'Add a role',
-		'Add an employee',
-		new inquirer.Separator(),
-		'Update employee role',
-		new inquirer.Separator(),
-		'exit',
-		new inquirer.Separator(),
 	];
 
+	// Add manage's options
+	if (ROLE?.includes('manager')) {
+		const opts = [
+			'Add a department',
+			'Add a role',
+			'Add an employee',
+			new inquirer.Separator(),
+			'Update employee role',
+			new inquirer.Separator(),
+		]
+		choices = [...choices, ...opts];
+	}
 
-	if (State.getInstance().get(EState.ROLE)?.includes('manager')) {
-		const managerOpts = [
+	// Add store manager and general manager options
+	if (ROLE === 'store manager' || ROLE === 'general manager') {
+		const opts = [
 			'Delete department',
 			'Delete role',
 			'Delete employee',
 			new inquirer.Separator(),
 		]
-		choices = [...choices, ...managerOpts];
+		choices = [...choices, ...opts];
 	}
+
+	// Exit option always at the end of the list
+	choices = [
+		...choices,
+		'exit',
+		new inquirer.Separator(),
+	]
 
 	return [
 		{
