991ce8f709f5c751077f117889bb4b54acfc507e
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-08-15 18:34:58 -0400

    update: depmnt controller with improved queries

--- src/controller/DepartmentController.ts
+++ src/controller/DepartmentController.ts
@@ -1,4 +1,5 @@
 import Department from '../model/Department';
+import State, { EState } from '../store/state';
 import Controller from './Controller';
 
 export default class DepartmentController extends Controller implements CRUD {
@@ -20,13 +21,21 @@ export default class DepartmentController extends Controller implements CRUD {
 	 * @throws {Error} Department is not defined
 	 */
 	async create(): Promise<Department> {
+		const STORE = State.getInstance();
+
 		if (!this.department) throw new Error('Department is not defined');
 		try {
 			const values = [this.department.name];
-			const query = 'INSERT INTO department (name) VALUES ($1) RETURNING *;';
-			const results = await this.fetch(query, values);
+			const createQuery = 'INSERT INTO department (name) VALUES ($1) RETURNING *;';
+			const transactionQuery = `
+			INSERT INTO department_transactions (department_id, created_by, updated_by)
+			VALUES ($1, $2, $3);
+			`;
+			const results = await this.fetch(createQuery, values);
 			const row = results.rows[0];
 			this.department.id = row.id;
+
+			await this.fetch(transactionQuery, [row.id, parseInt(STORE.get(EState.USER_ID)!), parseInt(STORE.get(EState.USER_ID)!)]);
 			return new Department(row.name, row.id, row.created_at, row.updated_at).toObject();
 		} catch (error) {
 			const ERROR = <Error>error;
@@ -42,10 +51,20 @@ export default class DepartmentController extends Controller implements CRUD {
 	async readOne(id: number): Promise<Department> {
 		try {
 			const values = [id];
-			const query = 'SELECT * FROM department WHERE id=$1;';
+			const query = `
+			SELECT department_transactions.id, department_transactions.created_at, department_transactions.updated_at,
+				CONCAT(created_by.first_name, ' ', created_by.last_name) AS created_by_full_name,
+				CONCAT(updated_by.first_name, ' ', updated_by.last_name) AS updated_by_full_name,
+				department.name AS department_name
+			FROM department_transactions
+			JOIN employee AS created_by ON department_transactions.created_by = created_by.id
+			JOIN employee AS updated_by ON department_transactions.updated_by = updated_by.id
+			JOIN department ON department_transactions.department_id = department.id
+			WHERE department.id = $1;
+			`;
 			const results = await this.fetch(query, values);
 			const row = results.rows[0];
-			return new Department(row.name, row.id, new Date(row.created_at), new Date(row.updated_at)).toObject();
+			return new Department(row.department_name, row.id, new Date(row.created_at), new Date(row.updated_at), row.created_by_full_name, row.updated_by_full_name).toObject();
 		} catch (error) {
 			const ERROR = <Error>error;
 			throw new Error(ERROR.message);
@@ -59,9 +78,18 @@ export default class DepartmentController extends Controller implements CRUD {
 	 */
 	async readAll(): Promise<Array<Department>> {
 		try {
-			const query = 'SELECT * FROM department;';
+			const query = `
+			SELECT department_transactions.id, department_transactions.created_at, department_transactions.updated_at,
+				CONCAT(created_by.first_name, ' ', created_by.last_name) AS created_by_full_name,
+				CONCAT(updated_by.first_name, ' ', updated_by.last_name) AS updated_by_full_name,
+				department.name AS department_name
+			FROM department_transactions
+			JOIN employee AS created_by ON department_transactions.created_by = created_by.id
+			JOIN employee AS updated_by ON department_transactions.updated_by = updated_by.id
+			JOIN department ON department_transactions.department_id = department.id;
+			`;
 			const results = await this.fetch(query);
-			return results.rows.map((row) => new Department(row.name, row.id, new Date(row.created_at), new Date(row.updated_at)).toObject());
+			return results.rows.map((row) => new Department(row.department_name, row.id, new Date(row.created_at), new Date(row.updated_at), row.created_by_full_name, row.updated_by_full_name).toObject());
 		} catch (error) {
 			const ERROR = <Error>error;
 			throw new Error(ERROR.message);
@@ -76,11 +104,18 @@ export default class DepartmentController extends Controller implements CRUD {
 	 * @throws {Error} Department is not defined
 	 */
 	async update(fields: Partial<DepartmentUpdatable>): Promise<Department> {
+		const STATE = State.getInstance();
 		if (!this.department) throw new Error('Department is not defined');
 		try {
 			const values = [fields.name ?? this.department.name, this.department.id];
-			const query = 'UPDATE department SET name = COALESCE($1, name) WHERE id=$2 RETURNING *;';
-			await this.fetch(query, values);
+			const updateQuery = 'UPDATE department SET name = COALESCE($1, name) WHERE id=$2 RETURNING *;';
+			const updateTransactions = `
+			UPDATE department_transactions
+			SET updated_by = $1, updated_at TIMESTAMP
+			WHERE department_id = $2;
+			`
+			await this.fetch(updateQuery, values);
+			await this.fetch(updateTransactions, [parseInt(STATE.get(EState.USER_ID)!), this.department.id])
 			return this.readOne(this.department.id!);
 		} catch (error) {
 			const ERROR = <Error>error;
